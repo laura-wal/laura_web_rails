0fc7ce32f653bf65c57553e9e69aa0117bb4e49e
Laura Walpole <l.a.walpole@googlemail.com>
2015-06-02 10:32:17 +0100

    homework submission

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+
+  def homepage
+  end
+
+  def about
+  end
+
+  def done
+  end
+
+  def like
+  end
+
+  def contact
+  @email = "l.a.walpole@googlemail.com"
+
+  end
+
+
+
+end  
